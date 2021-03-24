6f9492a3069314c03a0462b7779cde89a37cac2d
NikithaMN-05 <77766697+NikithaMN-05@users.noreply.github.com>
2021-03-24 16:34:00 -0500

    new endpoint

---
+++ routes/mydata.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'narsing rao nikitha madhari' });
+});
+
+module.exports = router;
